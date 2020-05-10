8da254457b7310a6d56ba8d970259b5f5eaade5a
dxxhj <dxxhjk@126.com>
2020-05-10 23:49:44 +0800

    完成基础版

--- assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,19 +1,63 @@
 package com.bytedance.videoplayer;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
+import android.widget.VideoView;
+import android.widget.MediaController;
 
 public class MainActivity extends AppCompatActivity {
 
+    private VideoView videoView;
+    private int videoPosition = 0;
+    private boolean isPlaying = false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
-        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
-        Glide.with(this).load(url).into(imageView);
+
+        videoView = (VideoView)findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + this.getPackageName() + "/" + R.raw.japan);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setMediaPlayer(videoView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        videoPosition = videoView.getCurrentPosition();
+        isPlaying = videoView.isPlaying();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        videoView.seekTo(videoPosition);
+        if (isPlaying)videoView.start();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@Nullable Bundle state) {
+        if (state != null) {
+            state.putInt("position", videoPosition);
+            state.putBoolean("isPlaying", isPlaying);
+        }
+        super.onSaveInstanceState(state);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@Nullable Bundle state) {
+        super.onRestoreInstanceState(state);
+        if (state != null) {
+            int position = state.getInt("position");
+            boolean isplaying = state.getBoolean("isPlaying");
+            videoPosition = position;
+            videoView.seekTo(position);
+            isPlaying = isplaying;
+            if (isPlaying)
+                videoView.start();
+        }
     }
 }
